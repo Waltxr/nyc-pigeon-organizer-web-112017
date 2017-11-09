cabfb2ae15251c1f161785ea52333e47f6e1364c
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-09 19:57:29 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,11 +11,7 @@ def nyc_pigeon_organizer(data)
   pigeon_hash = {}
 
   names.each do |name|
-    idx = 0
-    while idx < data.keys.length
-      pigeon_hash[name].
-      idx += 1
-    end
+      pigeon_hash[name] = {}
   end
 # merge({data.keys[idx]=>[]})
   return pigeon_hash
