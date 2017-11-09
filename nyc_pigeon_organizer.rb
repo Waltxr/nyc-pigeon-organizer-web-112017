ec272a8151ce2d68ab261eb1ee9048a6934b7f0a
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-09 18:57:03 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 def nyc_pigeon_organizer(data)
   array_of_keys = []
   data.each do |key, value|
+    key.each do |attribute, names| 
     array_of_keys << value
+    end
   end
   return array_of_keys
 end
